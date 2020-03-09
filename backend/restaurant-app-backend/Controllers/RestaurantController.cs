995ff7bd460da77c4615854f0c05ecdae5b57200
itsercleung <leung.eric24@gmail.com>
2020-03-09 17:49:05 +1300

    init

---
+++ backend/restaurant-app-backend/Controllers/RestaurantController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace restaurant_app_backend.Controllers
+{
+    [ApiController]
+    [Route("test")]
+    public class RestaurantController : ControllerBase
+    {
+        private static readonly string[] Names = new[]
+        {
+            "Restaurant 1", "Restaurant 2"
+        };
+
+        private readonly ILogger<RestaurantController> _logger;
+
+        public RestaurantController(ILogger<RestaurantController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String[] Get()
+        {
+            return Names;
+        }
+    }
+}
